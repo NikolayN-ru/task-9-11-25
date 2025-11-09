76d8a921485e7c5685e41151be25626fe091a927
NikolayN-ru <NikolayN-ru@users.noreply.github.com>
2025-11-09 16:57:42 +0000

    deploy: d3b548b76e8b294ce31f979188b2b7d09da59a0b

---
+++ _next/static/nLfyu--alj5frcKokf5nB/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/task-9-11-25//_next/:path+",
+        "destination": "/task-9-11-25/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
